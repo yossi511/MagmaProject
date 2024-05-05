6e9c8719b26efe58909bddcbc70eb32d23288e10
Yossi Landa <yossilan@post.bgu.ac.il>
2024-05-05 23:17:56 +0300

    Small addition
    Some changes that i've forgotten to push.

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import pika
 import json
 from elasticsearch import Elasticsearch
 
-# Connection to the RabbitMQ server
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-
 # Connection to the Elasticsearch index
 es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
 
@@ -17,7 +13,7 @@ images_directory = "images"
 # Creating a document in the Elasticsearch index with a link to the image
 # TODO: Add image handling
 def save_to_elasticsearch(pokemon, image_path):
-    #pokemon['image_path'] = image_path
+    # pokemon['image_path'] = image_path
     es.index(index='pokemon_index', body=pokemon)
 
 
@@ -48,9 +44,67 @@ def save_image(pokemon_name, image_data):
     return image_path
 
 
-# Setting up conditions for receiving messages from RabbitMQ
-channel.queue_declare(queue='pokemon_queue', durable=True)
-channel.basic_consume(queue='pokemon_queue', on_message_callback=callback, auto_ack=True)
+# Perform a search query to retrieve Pokémon
+def search_pokemon(query):
+    result = es.search(index='pokemon_index',
+                       body={"query": {"match": {"name": {"query": query, "fuzziness": "AUTO"}}}})
+    return result['hits']['hits']
 
-print('Waiting for messages...')
-channel.start_consuming()
+
+# Function to create an Elasticsearch index
+def create_pokemon_index():
+    # Define the index settings and mappings
+    index_settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0
+        },
+        "mappings": {
+            "properties": {
+                "name": {"type": "text"},
+                "level": {"type": "integer"},
+                "type": {"type": "keyword"},
+                "image_url": {"type": "text"}
+            }
+        }
+    }
+
+    # Name of the index
+    index_name = "pokemon_index"
+
+    # Check if the index already exists
+    if not es.indices.exists(index=index_name):
+        # Create the index
+        es.indices.create(index=index_name, body=index_settings)
+        print(f"Index '{index_name}' created successfully.")
+    else:
+        print(f"Index '{index_name}' already exists.")
+
+
+# Main function to execute the search
+def main():
+    create_pokemon_index()
+
+    while True:
+        query = input("Enter a Pokémon name or part of a name: (Enter exit in order to exit)")
+        if query.lower() == 'exit':
+            break
+        pokemon_hits = search_pokemon(query)
+        print("Search results:")
+        for hit in pokemon_hits:
+            print(hit['_source'])
+
+    # Connection to the RabbitMQ server
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Setting up conditions for receiving messages from RabbitMQ
+    channel.queue_declare(queue='pokemon_queue', durable=True)
+    channel.basic_consume(queue='pokemon_queue', on_message_callback=callback, auto_ack=True)
+
+    print('Waiting for messages...')
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    main()
